43ed6e46d2b84a4fcdedf2c1b4fdc7a5c0a91397
Daemon Jung <jjjssswww1006@gmail.com>
2024-05-28 01:07:22 +0900

    build: viewBinding 추가 반영

--- app/src/main/java/com/smumc/smumc_6th_teamc_android/mypage/MypageActivity.kt
+++ app/src/main/java/com/smumc/smumc_6th_teamc_android/mypage/MypageActivity.kt
@@ -1,4 +1,15 @@
 package com.smumc.smumc_6th_teamc_android.mypage
 
-class MypageActivity {
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.smumc.smumc_6th_teamc_android.databinding.ActivityMypageBinding
+
+class MypageActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivityMypageBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMypageBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
 }
