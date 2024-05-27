c098b6adcc744bfec05207b385551d817db2bbc8
Daemon Jung <jjjssswww1006@gmail.com>
2024-05-27 23:35:01 +0900

    initial commit

---
+++ app/src/main/java/com/smumc/smumc_6th_teamc_android/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.smumc.smumc_6th_teamc_android
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
