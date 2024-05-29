d0d74d4a76fd430665a41b971cabfb4d525a13a5
rlagns1234 <126846654+rlagns1234@users.noreply.github.com>
2024-05-29 16:12:51 +0900

    feat: 채팅 레이아웃 구성

--- app/src/main/java/com/smumc/smumc_6th_teamc_android/MainActivity.kt
+++ app/src/main/java/com/smumc/smumc_6th_teamc_android/MainActivity.kt
@@ -1,15 +1,24 @@
 package com.smumc.smumc_6th_teamc_android
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.smumc.smumc_6th_teamc_android.chat.ChatActivity
 import com.smumc.smumc_6th_teamc_android.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
     lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        // ChatActivity를 DISPLAY_MODE 값 1을 전달하며 시작
+        val intent = Intent(this, ChatActivity::class.java).apply {
+            putExtra("DISPLAY_MODE", 1)
+        }
+        startActivity(intent)
     }
 }
